55a3bf30dec46ded7d376a94ce8192fe5dc2a295
amirh <amirh@DESKTOP-M08CGOK>
2024-08-09 12:51:22 +0330

    Add project files.

---
+++ tamrin1/Program.cs
@@ -0,0 +1,19 @@
+﻿namespace tamrin1
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            string name, lastname;
+            Console.WriteLine("enter your name");
+            name = Console.ReadLine();
+
+
+
+
+
+
+
+        }
+    }
+}
